8af9c5f121b842f9a14d1b8f8a2cabc259ec7b07
gody48 <109515290+gody48@users.noreply.github.com>
2022-11-24 11:49:40 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export function fibonacci(num: number): number | null {
     return null;
   }
 
-  let a = 42, b = 0, temp;
+  let a = 42, b = 0, c = 2, d = 9, e = 20, temp;
   a = 1;
 
   while (num >= 0){
